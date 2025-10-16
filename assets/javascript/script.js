91d7ffce56ed1dff3af731322696640c0edbff16
Davide De Masi <demasi.dav@gmail.com>
2025-10-16 20:32:24 +0200

    aggiunta scomparsa del menu ham dopo aver cliccato su un link

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,3 +9,12 @@ document.addEventListener('scroll', ()=>{
     }
 });
 // ----------------------------------------------------------
+// chiusura menu hamburger appena cliccato
+
+const menu= document.querySelector('.menu-ham');
+const alink=document.querySelectorAll('.menu-list a');
+alink.forEach(link=>{
+    link.addEventListener('click', ()=>{
+        menu.removeAttribute('open');
+   })
+});
